a59ee6ad5de8eec661d8df625633ddebb3fa95e1
Tiago Bras <tiagodsbras@gmail.com>
2016-07-14 13:16:18 +0100

    Deletes test.py and bumps version to v0.1.1

--- setup.py
+++ setup.py
@@ -6,10 +6,19 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+def version():
+    with open(os.path.abspath('VERSION')) as f:
+        return f.read().strip()
+    
+    raise IOError("Error: 'VERSION' file not found.")
+
+VERSION = version()
+
 setup(
 	name='PyBuddy',
- 	version='0.0.1',
-	description='',
+ 	version=VERSION,
+	description='Creates and setups a well structured Python project',
+    long_description=open(os.path.abspath('README.md')).read(),
 	author='Tiago Bras',
 	author_email='tiagodsbras@gmail.com',
 	license='MIT',
